7b7da6f85cc0535df1729985ffd0e5888074717d
Klathmon <gregbenner1@gmail.com>
2016-08-27 18:14:11 -0400

    start adding test stuff and clean up the root directory

---
+++ test/log.spec.js
@@ -0,0 +1,9 @@
+import { logError } from '../src/log'
+
+describe('Log module', () => {
+  describe('startCommand()', () => {
+    it('should return an end function')
+    it('should print a starting line')
+    it('should finish and print a reasonable time')
+  })
+})
