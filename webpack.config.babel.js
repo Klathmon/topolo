72c712f45232b4f0b65e5ba7f7e706df94e116d3
Klathmon <gregbenner1@gmail.com>
2016-08-26 21:33:47 -0400

    start working on webpack build

---
+++ webpack.config.babel.js
@@ -0,0 +1,44 @@
+import webpack from 'webpack'
+import path from 'path'
+import ProgressBarPlugin from 'progress-bar-webpack-plugin'
+
+export default {
+  target: 'node',
+  entry: path.resolve(__dirname, 'src', 'topolo.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'topolo.js'
+  },
+  plugins: [
+    new ProgressBarPlugin(),
+    new webpack.LoaderOptionsPlugin({ debug: false, minimize: true }),
+    new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /en/),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false,
+        screw_ie8: true,
+        unsafe: true,
+        collapse_vars: true,
+        pure_getters: true,
+        keep_fargs: false
+      },
+      output: {
+        comments: false
+      }
+    })
+  ],
+  module: {
+    loaders: [{
+      test: /\.js$/i,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      query: {
+        presets: [
+          ['es2015', { modules: false }],
+          'stage-1'
+        ]
+      }
+    }]
+  }
+}
