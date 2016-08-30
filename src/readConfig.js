fc43cf74da38b65bd55a8a389d08b607bc3eddef
Klathmon <gregbenner1@gmail.com>
2016-08-29 20:09:27 -0400

    fix webpack issue

--- src/readConfig.js
+++ src/readConfig.js
@@ -33,7 +33,7 @@ export default async function readConfig (configFile) {
     // Pull out the sub-object from a package.json if we are reading one, otherwise use the whole file
     config = (programName in rawConfig ? rawConfig[programName] : rawConfig)
   } catch (err) {
-    config = require(configFileFullPath)
+    config = window['require'](configFileFullPath) // gotta trick webpack...
   }
   // map over the config's values and set defaults for any missing params
   return applyDefaults(config)
