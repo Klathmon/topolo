2399869538189e1cef4935fa631fd34f7d25b973
Gregory Benner <gregbenner1@gmail.com>
2016-08-30 21:22:13 -0400

    add runCommand function

---
+++ newSrc/runCommand.js
@@ -0,0 +1,32 @@
+import { delimiter, resolve as pathResolve } from 'path'
+import spawn from 'cross-spawn'
+import spawnArgs from 'spawn-args'
+
+export function runCommand (command = null, env = {}) {
+  if (command === null) {
+    return Promise.resolve()
+  } else {
+    return new Promise((resolve, reject) => {
+      const args = spawnArgs(command, { removequotes: 'always' })
+      const proc = spawn(args.shift(), args, {
+        stdio: 'inherit',
+        env: {
+          ...process.env,
+          PATH: process.env.PATH + delimiter + pathResolve('.', 'node_modules', '.bin'),
+          ...env
+        }
+      })
+
+      proc.on('error', (err) => reject(err))
+
+      proc.on('exit', (code) => {
+        // TODO: Handle exit code here
+        if (parseInt(code, 10) === 0) {
+          resolve()
+        } else {
+          reject(new Error(`Non-zero exit code of "${code}"`))
+        }
+      })
+    })
+  }
+}
