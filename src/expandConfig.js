5b4338c32bdbe07db75c3b5a27b130dabf674031
Klathmon <gregbenner1@gmail.com>
2016-09-03 18:47:58 -0400

    add more tests, and fix even more bugs in expandConfig

--- src/expandConfig.js
+++ src/expandConfig.js
@@ -1,4 +1,4 @@
-import { includes, mapValues, isObject, isFunction, isArray, get } from 'lodash'
+import { includes, mapValues, isObject, isFunction, isArray, isString, get } from 'lodash'
 
 import {
   COMMAND_KEY,
@@ -33,23 +33,23 @@ function expandTask (task) {
       [ANYTIME_KEY]: []
     }
   }
-  if (isObject(task) && !isFunction(task) && !isArray(task)) {
+  if (isString(task) || isFunction(task) || isArray(task)) {
+    expandedTask[COMMAND_KEY] = task
+  } else {
     expandedTask[COMMAND_KEY] = get(task, COMMAND_KEY, [])
     expandedTask[ENV_KEY] = get(task, ENV_KEY, {})
     if (DEPENDENCIES_KEY in task) {
       const dependencies = task[DEPENDENCIES_KEY]
-      if (isObject(dependencies)) {
+      if (isString(dependencies) || isArray(dependencies)) {
+        expandedTask[DEPENDENCIES_KEY][BEFORE_KEY] = wrapInArray(dependencies)
+      } else {
         for (let property of [BEFORE_KEY, OPTIONAL_BEFORE_KEY, AFTER_KEY, OPTIONAL_AFTER_KEY, ANYTIME_KEY]) {
           if ((property in dependencies)) {
             expandedTask[DEPENDENCIES_KEY][property] = wrapInArray(dependencies[property])
           }
         }
-      } else {
-        expandedTask[DEPENDENCIES_KEY][BEFORE_KEY] = wrapInArray(dependencies)
       }
     }
-  } else {
-    expandedTask[COMMAND_KEY] = task
   }
 
   return expandedTask
--- test/expandConfig.spec.js
+++ test/expandConfig.spec.js
@@ -34,17 +34,37 @@ describe('expandConfig.js', () => {
       expect(task).to.have.property(DEPENDENCIES_KEY).that.has.property(OPTIONAL_AFTER_KEY).that.is.instanceof(Array)
       expect(task).to.have.property(DEPENDENCIES_KEY).that.has.property(ANYTIME_KEY).that.is.instanceof(Array)
     })
-    it('expands a task that is a string', () => {
+    it('expands a simple command that is a string', () => {
       const task = 'test'
       expect(expandTask(task)).to.have.property(COMMAND_KEY).that.equals(task)
     })
-    it('expands a task that is a function', () => {
+    it('expands a simple command that is a function', () => {
       const task = () => null
       expect(expandTask(task)).to.have.property(COMMAND_KEY).that.equals(task)
     })
-    it('expands a task that is an array', () => {
+    it('expands a simple command that is an array', () => {
       const task = ['test1', 'test2']
       expect(expandTask(task)).to.have.property(COMMAND_KEY).that.equals(task)
     })
+    for (let [type, val] of [['a string', 'test'], ['an array', ['test1', 'test2']]]) {
+      it(`expands a bare "${DEPENDENCIES_KEY}" key that is ${type}`, () => {
+        expect(expandTask({ [DEPENDENCIES_KEY]: val }))
+          .to.have.property(DEPENDENCIES_KEY)
+          .that.has.property(BEFORE_KEY)
+          .that.is.instanceof(Array)
+          .that.has.length.above(0)
+      })
+    }
+    for (let property of [BEFORE_KEY, OPTIONAL_BEFORE_KEY, AFTER_KEY, OPTIONAL_AFTER_KEY, ANYTIME_KEY]) {
+      for (let [type, val] of [['a string', 'test'], ['an array', ['test1', 'test2']]]) {
+        it(`expands the "${property}" key that is ${type}`, () => {
+          expect(expandTask({ [DEPENDENCIES_KEY]: { [property]: val } }))
+            .to.have.property(DEPENDENCIES_KEY)
+            .that.has.property(property)
+            .that.is.instanceof(Array)
+            .that.has.length.above(0)
+        })
+      }
+    }
   })
 })
