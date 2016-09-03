8cd9419f072718ea682ed0af300f20a3004cc1f7
Klathmon <gregbenner1@gmail.com>
2016-09-03 14:55:56 -0400

    get rid of oldSrc

--- oldSrc/configKeys.js
+++
@@ -1,9 +0,0 @@
-
-export const COMMAND = 'command'
-export const ENV = 'env'
-export const DEFAULT_VALUES = 'defaultValues'
-export const REQ_ANYTIME = 'anytime'
-export const REQ_BEFORE = 'before'
-export const OPT_BEFORE = 'optionalBefore'
-export const REQ_AFTER = 'after'
-export const OPT_AFTER = 'optionalAfter'
