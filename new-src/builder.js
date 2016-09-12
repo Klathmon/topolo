9988b2784267dc8c3449c39506e99e13109a7efc
Gregory Benner <gregbenner1@gmail.com>
2016-09-11 20:47:02 -0400

    attempt to get an addTask together

--- new-src/builder.js
+++ new-src/builder.js
@@ -1,7 +1,35 @@
 
-export const taskObject = {}
+export const tasks = {}
 
-export function addTask (taskName, task) {
-  taskObject[taskName] = task
-  taskObject[taskName].opt = 'stuff'
+/**
+ * Task Types:
+ * string - command
+ * parallel-array-obj - run in parallel
+ * seq-array-obj - run in seq
+ */
+
+/**
+ * Task option flags:
+ * optional
+ * silent/quiet
+ * non-blocking?
+ *
+ */
+
+export function addTask (taskName, taskGenerator) {
+  const task = function (...args) {
+    this.task = taskGenerator(...args)
+  }
+  Object.defineProperties(task, {
+    get optional () {
+      this.flags.optional = true
+      return this
+    },
+    get silent () {
+      this.flags.silent = true
+      return this
+    }
+  })
+
+  tasks[taskName] = task
 }
